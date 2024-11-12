59dd4f872d08e4108d86a9cecd494ac73a3e6aa6
Cantoara <advcantoara@gmail.com>
2024-11-12 11:07:18 -0300

    Teste Beck End

---
+++ Operacoes.cs
@@ -0,0 +1,18 @@
+﻿
+namespace TesteCore
+{
+    public static class Operacoes
+    {
+        public static double Somar(double primeiroNumero, double segundoNumero)
+        {
+            return (primeiroNumero + segundoNumero);
+        }
+
+        public static double Multiplicar(double primeiroNumero, double segundoNumero)
+        {
+            return (primeiroNumero * segundoNumero);
+        }
+
+    }
+
+}
